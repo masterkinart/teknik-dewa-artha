3e158e395ec279984926c59ab268c23d9e2168d2
masterkinart <kinart.master@gmail.com>
2026-08-03 11:44:25 +0700

    update sitemap 3

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h2>Halaman tidak ditemukan</h2>
-      <p>Maaf, halaman yang Anda cari tidak tersedia.</p>
+      <p>Maaf, halaman yang Anda cari tidak tersedia untuk saat ini.</p>
     </div>
   );
 }
